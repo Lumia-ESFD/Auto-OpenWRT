f57341d9c1fb2501dfce1f2ba6d311d7c80c177d
Lumia-ESFD <82859034+Lumia-ESFD@users.noreply.github.com>
2022-05-14 19:51:55 +0800

    Add files via upload

---
+++ MiR4A/clone_source.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git clone "https://github.com/coolsnowwolf/lede.git"
+
+pushd "lede"; 
+
+export shanchu1=$(grep  -a -n -e '&spi0 {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(grep  -a -n -e '&pcie {' target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi|cut -d ":" -f 1)
+export shanchu2=$(expr $shanchu2 - 1)
+export shanchu2=$(echo $shanchu2"d")
+sed -i $shanchu1,$shanchu2 target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+grep  -Pzo '&spi0[\s\S]*};[\s]*};[\s]*};[\s]*};' target/linux/ramips/dts/mt7621_youhua_wr1200js.dts > youhua.txt
+echo "" >> youhua.txt
+echo "" >> youhua.txt
+export shanchu1=$(expr $shanchu1 - 1)
+export shanchu1=$(echo $shanchu1"r")
+sed -i "$shanchu1 youhua.txt" target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+rm -rf youhua.txt
+
+export imsize1=$(grep  -a -n -e 'define Device/xiaomi_mir3g-v2' target/linux/ramips/image/mt7621.mk|cut -d ":" -f 1)
+export imsize1=$(expr $imsize1 + 2)
+export imsize1=$(echo $imsize1"s")
+sed -i "$imsize1/IMAGE_SIZE := .*/IMAGE_SIZE := 16064k/" target/linux/ramips/image/mt7621.mk
+
+sed -i 's/ssid=OpenWrt/ssid=MIWIFI/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=Password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+./scripts/feeds update -a -f
+./scripts/feeds install -a -f
+
+popd
