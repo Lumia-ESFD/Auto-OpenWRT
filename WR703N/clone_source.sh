6a123a07081da977636e1c2f469267783f72718d
Lumia-ESFD <82859034+Lumia-ESFD@users.noreply.github.com>
2022-05-20 22:28:22 +0800

    update

--- WR703N/clone_source.sh
+++ WR703N/clone_source.sh
@@ -5,10 +5,12 @@ git clone "https://github.com/coolsnowwolf/lede.git" OpenWRT
 pushd "OpenWRT"; 
 
 git reset --hard e2535799fbfa1806694a5c5699f1bbe842868771
-sed -i -e 's/192.168.1.1/192.168.3.1/g' -e 's/OpenWrt/TPLink_WR703Nv1/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
+#sed -i -e 's/192.168.1.1/192.168.3.1/g' -e 's/OpenWrt/TPLink_WR703Nv1/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
+sed -i -e 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate 
 sed -i '343s/4/16/g' target/linux/ar71xx/image/tiny-tp-link.mk  #修改wr703nv1的编译固件大小
-sed -i -e 's/OpenWrt/TPLink_WR703Nv1/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i -e 's/OpenWrt/TPLink_WR703Nv1/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改WiFi名称,添加加密方式和密码
+-e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
 sed -i -e '1s/$/^e82d730/g' feeds.conf.default
